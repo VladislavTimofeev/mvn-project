b58df8b94602383554813ba447dbedd7f2c9f02b
Vladislav Timofeev <timofeev.vladislav.sergeevich@gmail.com>
2024-10-21 17:29:20 +0200

    Added spring, created spring beans

--- service/src/main/java/com/vlad/repository/impl/RequestRepository.java
+++ service/src/main/java/com/vlad/repository/impl/RequestRepository.java
@@ -3,7 +3,9 @@ package com.vlad.repository.impl;
 import com.vlad.entity.Request;
 import com.vlad.repository.AbstractRepository;
 import jakarta.persistence.EntityManager;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class RequestRepository extends AbstractRepository<Long, Request> {
 
     public RequestRepository(EntityManager entityManager) {
--- service/src/main/java/com/vlad/repository/impl/TripRepository.java
+++ service/src/main/java/com/vlad/repository/impl/TripRepository.java
@@ -3,7 +3,9 @@ package com.vlad.repository.impl;
 import com.vlad.entity.Trip;
 import com.vlad.repository.AbstractRepository;
 import jakarta.persistence.EntityManager;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class TripRepository extends AbstractRepository<Long, Trip> {
 
     public TripRepository(EntityManager entityManager) {
