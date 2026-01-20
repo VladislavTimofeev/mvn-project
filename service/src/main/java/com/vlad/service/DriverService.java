23b79e6e9147a949207c801eac2c499eb88f8d31
VladislavTimofeev <timofeev.vladislav.sergeevich@gmail.com>
2026-01-20 15:15:54 +0300

    smallfix

--- service/src/main/java/com/vlad/service/DriverService.java
+++ service/src/main/java/com/vlad/service/DriverService.java
@@ -7,7 +7,6 @@ import com.vlad.dto.filter.DriverFilterDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface DriverService {
